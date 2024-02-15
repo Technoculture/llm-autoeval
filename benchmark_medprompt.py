7c16327e8d7eafcf7671fd39a7b40a2275bdfc81
dkshjn <dakshjain17@gmail.com>
2024-02-16 04:33:54 +0530

    Formatting changes in all files and minor change in specifying parameters.

--- benchmark_medprompt.py
+++ benchmark_medprompt.py
@@ -3,20 +3,31 @@ from benchmark import test
 from medprompt import MedpromptModule
 
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type= str, default="gpt-3.5-turbo", help="Model to be used.")
-    parser.add_argument("--api_key", type=str, help="YOUR_API_KEY")
-    parser.add_argument("--benchmark", type=str, help = "Choose one of the following benchmark: [medmcqa, medicationqa, mmlu_medical, mmlu_general, arc, hellaswag, winogrande, blurb, truthfulqa, gsm8k].", default="arc")
-    parser.add_argument("--shots", type=int, help = "Number of few shots.", default=5)
-    parser.add_argument("--dspy_module", type = str, help = "Name of dspy module.", default="medprompt")
-    args = parser.parse_args()
-
-    results = test(
-    model="google/flan-t5-base",
-    api_key = "",
-    dspy_module=MedpromptModule,
-    benchmark="arc",
-    shots=5,
+def main(args):
+    test(
+        model=args.model,
+        api_key=args.api_key,
+        dspy_module=args.dspy_module,
+        benchmark=args.benchmark,
+        shots=args.shots,
     )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model", type=str, default="google/flan-t5-base", help="Model to be used."
+    )
+    parser.add_argument("--api_key", type=str, help="YOUR_API_KEY")
+    parser.add_argument(
+        "--benchmark",
+        type=str,
+        help="Choose one of the following benchmark: [medmcqa, medicationqa, mmlu_medical, mmlu_general, arc, hellaswag, winogrande, blurb, truthfulqa, gsm8k].",
+        default="arc",
+    )
+    parser.add_argument("--shots", type=int, help="Number of few shots.", default=5)
+    parser.add_argument(
+        "--dspy_module", type=MedpromptModule, help="Name of dspy module.", default=MedpromptModule
+    )
+    args = parser.parse_args()
+    main(args)
--- evaluate.py
+++ evaluate.py
@@ -1,26 +1,21 @@
 import argparse
-import openai
 import dspy
 from benchmark import benchmark_factory
 import logging
-import re
-import random
 from tqdm import tqdm
-from dspy.teleprompt import KNNFewShot
-from dspy.predict.knn import KNN
-from dspy.teleprompt.teleprompt import Teleprompter
+
 
 def model_setting(model_name, API_KEY):
-
-    model=dspy.OpenAI(model=model_name, api_key=API_KEY)
+    model = dspy.OpenAI(model=model_name, api_key=API_KEY)
     dspy.settings.configure(lm=model)
     return model
+
 
 def hfmodel_setting(model_name):
-
-    model=dspy.HFModel(model=model_name)
+    model = dspy.HFModel(model=model_name)
     dspy.settings.configure(lm=model)
     return model
+
 
 def answer_prompt(prompts, model):
     responses = []
@@ -30,53 +25,57 @@ def answer_prompt(prompts, model):
         responses.append(generated_response)
     return responses
 
-def benchmark_preparation(benchmark_obj, args):
 
+def benchmark_preparation(benchmark_obj, args):
     partition = "test" if "test" in benchmark_obj.splits else "validation"
     benchmark_obj.load_data(partition=partition)
     benchmark_obj.preprocessing(partition=partition)
     if args.shots > 0:
-            logging.info('Loading train data for few shot learning')
-            benchmark_obj.load_data(partition='train')
-            benchmark_obj.preprocessing(partition='train')
-            logging.info(f'FEW SHOTS: {args.shots}')
-            benchmark_obj.add_few_shot(
-                    shots=args.shots)
+        logging.info("Loading train data for few shot learning")
+        benchmark_obj.load_data(partition="train")
+        benchmark_obj.preprocessing(partition="train")
+        logging.info(f"FEW SHOTS: {args.shots}")
+        benchmark_obj.add_few_shot(shots=args.shots)
+
 
 def evaluate_model(benchmark_instance, model):
-
     predictions = answer_prompt(benchmark_instance.test_data["prompt"], model)
     evaluate_predictions(predictions, benchmark_instance.test_data["gold"])
 
+
 def evaluate_predictions(pred, ref):
-
-
     correct = sum(1 for pred_letter, truth in zip(pred, ref) if pred_letter[0] == truth)
     total = len(ref)
-    accuracy = (correct / total)
+    accuracy = correct / total
     print(f"Accuracy: {accuracy:.2%}")
 
-def main(args):
 
+def main(args):
     if args.model == "gpt-3.5-turbo" or args.model == "gpt-4":
         model = model_setting(args.model, args.api_key)
     else:
         model = hfmodel_setting(args.model)
 
-    #Creating a benchmark instance, loading data and processing.
+    # Creating a benchmark instance, loading data and processing.
     benchmark_instance = benchmark_factory(args.benchmark)
     benchmark_preparation(benchmark_instance, args)
 
-    #Evaluating
+    # Evaluating
     evaluate_model(benchmark_instance, model)
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type= str, default="gpt-3.5-turbo", help="Model to be used.")
+    parser.add_argument(
+        "--model", type=str, default="gpt-3.5-turbo", help="Model to be used."
+    )
     parser.add_argument("--api_key", type=str, help="YOUR_API_KEY")
-    parser.add_argument("--benchmark", type=str, help = "Choose one of the following benchmark: [medmcqa, medicationqa, mmlu_medical, mmlu_general, arc, hellaswag, winogrande, blurb, truthfulqa, gsm8k].", default="arc")
-    parser.add_argument("--shots", type=int, help = "Number of few shots.", default=0)
+    parser.add_argument(
+        "--benchmark",
+        type=str,
+        help="Choose one of the following benchmark: [medmcqa, medicationqa, mmlu_medical, mmlu_general, arc, hellaswag, winogrande, blurb, truthfulqa, gsm8k].",
+        default="arc",
+    )
+    parser.add_argument("--shots", type=int, help="Number of few shots.", default=0)
     args = parser.parse_args()
     main(args)
